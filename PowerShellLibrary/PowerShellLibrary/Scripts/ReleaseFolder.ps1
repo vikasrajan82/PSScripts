e6fdafe11d1514921d60039c6272fdbec610e62b
Vikas Rajan <virajan@microsoft.com>
2020-04-07 22:45:20 +0530

    Power Shell scripts

---
+++ PowerShellLibrary/PowerShellLibrary/Scripts/ReleaseFolder.ps1
@@ -0,0 +1,56 @@
+﻿# Starter pipeline
+# Start with a minimal pipeline that you can customize to build and deploy your code.
+# Add steps that build, run tests, deploy, and more:
+# https://aka.ms/yaml
+
+parameters:
+  - name: releasefolder
+    displayName: Release Folder
+    type: string
+
+trigger: none
+
+name: ${{parameters.releasefolder}}-$(date:yyyyMMdd)$(rev:.r)
+
+stages:
+- stage: Build
+  jobs:
+  - job: PackContents
+    displayName: Release Folder
+    pool:
+      vmImage: 'ubuntu-latest'
+
+    steps:
+    - task: PowerShell@2
+      displayName: Validate Release Folder
+      inputs:
+        targetType: 'inline'
+        script: |
+          if ( -not (Test-Path -Path '$(Build.SourcesDirectory)/Nitrogen/Releases/${{parameters.releasefolder}}/Deployment' -PathType Container))
+          {
+            Write-Host "The specified release folder (/Nitrogen/Releases/${{parameters.releasefolder}}/Deployment) is not a directory. Please verify"
+            exit 1
+          }
+          else 
+          {
+            Write-Host "The specified release folder (/Nitrogen/Releases/${{parameters.releasefolder}}/Deployment) is valid. Proceeding with the deployment."
+          }
+    - task: CopyFiles@2
+      displayName: Copy Automation Utility Files
+      inputs:
+        SourceFolder: '$(Build.SourcesDirectory)/Nitrogen/Deployment Automation'
+        Contents: '**'
+        TargetFolder: '$(Build.ArtifactStagingDirectory)'
+    - task: CopyFiles@2
+      displayName: Copy Deploymnet Files
+      inputs:
+        SourceFolder: '$(Build.SourcesDirectory)/Nitrogen/Releases/${{parameters.releasefolder}}/Deployment'
+        Contents: '**'
+        TargetFolder: '$(Build.ArtifactStagingDirectory)'
+    - task: PublishBuildArtifacts@1
+      inputs:
+        pathtoPublish: '$(Build.ArtifactStagingDirectory)'
+        artifactName: drop
+
+      
+
---
+++ PowerShellLibrary/PowerShellLibrary/Scripts/SolutionExtraction.ps1
@@ -0,0 +1,72 @@
+﻿# Starter pipeline
+# Start with a minimal pipeline that you can customize to build and deploy your code.
+# Add steps that build, run tests, deploy, and more:
+# https://aka.ms/yaml
+
+name: $(BuildDefinitionName)-$(Date:yyyyMMdd).$(Rev:.r)
+
+trigger:
+- $(Build.SourceBranchName)
+
+pool:
+  vmImage: 'vs2017-win2016'
+
+steps:
+- script: md tools
+  displayName: 'Create tools directory'
+
+- powershell: |
+    Invoke-WebRequest `
+      -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe `
+      -OutFile tools\\nuget.exe
+  displayName: 'Download nuget.exe'
+
+- powershell: |
+    tools\\nuget.exe install Microsoft.CrmSdk.CoreTools -O tools
+    md "tools\\CoreTools"
+    $coreToolsFolder = Get-ChildItem tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.CoreTools.'}
+    move "tools\\$coreToolsFolder\\content\\bin\\coretools\\*.*" "tools\\CoreTools"
+    Remove-Item "tools\\$coreToolsFolder" -Force -Recurse
+  displayName: 'Install CoreTools'
+
+- powershell: |
+    $files = @(Get-ChildItem $(Build.Repository.LocalPath)\Nitrogen\Backups\Solutions\Raw\*.zip)
+    foreach ($file in $files) 
+    {
+        Start-Process tools/CoreTools/SolutionPackager.exe `
+        -ArgumentList `
+          "/action: Extract", `
+          "/zipfile: $file", `
+          "/folder: $(Build.Repository.LocalPath)\Nitrogen\Backups\Solutions\Extracted", `
+          "/allowDelete:No" `
+        -Wait `
+        -NoNewWindow
+    }
+  displayName: 'Solution Packager: pack solution'
+
+#- powershell: |
+#    Start-Process tools/CoreTools/SolutionPackager.exe `
+#    -ArgumentList `
+#      "/action: Extract", `
+#      "/zipfile: $(Build.Repository.LocalPath)\Nitrogen\Backups\Solutions\Raw\LenovoNitrogenConfiguration_1_0_0_0.zip", `
+#      "/folder: $(Build.Repository.LocalPath)\Nitrogen\Backups\Solutions\Extracted", `
+#      "/allowDelete:No" `
+#    -Wait `
+#    -NoNewWindow
+#  env:
+#    SolutionPath: $(solution.path)
+#    SolutionName: $(solution.name)
+#  displayName: 'Solution Packager: pack solution'
+
+- script: if exist tools rd /s /q tools
+  displayName: 'Delete tools folder exists'
+
+- powershell: |
+    git config --global user.email "virajan@microsoft.com"
+    git config --global user.name "Vikas Rajan"
+    git checkout $(Build.SourceBranchName)
+    git add -A
+    git status
+    git commit -m "Solution Extraction Automation"
+    git -c http.extraheader="AUTHORIZATION: bearer $(System.AccessToken)" push
+  displayName: 'Commit changes'
